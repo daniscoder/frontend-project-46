211340522c876eefab57dd8a907ea2eecffb04c1
Danis Arslanov <arslanovdk@gmail.com>
2024-05-23 12:42:47 +0500

    fix build full path

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import fs from 'fs';
 import parseData from './parsers.js';
 import compare from './compare.js';
 
+const fixturesPath = '__fixtures__';
+
 const getFilePathExtension = (filePath) => {
   const extname = path.extname(filePath);
-  return extname !== '' ? extname.split('.')[1] : '';
+  return extname !== '' ? extname.split('.').at(-1) : '';
 };
 
-const buildFullPath = (filePath) => path.resolve(process.cwd(), filePath);
+const buildFullPath = (filePath) => {
+  if (path.isAbsolute(filePath)) {
+    return filePath;
+  }
+  return path.resolve(process.cwd(), fixturesPath, path.basename(filePath));
+};
 
 const readTextFile = (filePath) => fs.readFileSync(buildFullPath(filePath)).toString();
 
